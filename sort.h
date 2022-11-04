529bd08a9c72928268fc9dbd89dd54cad3247263
Chineze Ogugua <christabelogugua@gmail.com>
2022-11-03 17:50:19 -0700

    sort header

--- sort.h
+++ sort.h
@@ -2,15 +2,15 @@
 #define SORT_H
 
 #include <stdio.h>
-#include "sort.h"
+#include <stdlib.h>
 
-/*
+/**
  * struct listint_s - Doubly linked list node
- * @n: Integer  stored in the node
+ *
+ * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
-
 typedef struct listint_s
 {
 	const int n;
@@ -18,15 +18,14 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* Prototypes for mandatory tasks */
+
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void quick_sort(int *array, size_t size);
 void selection_sort(int *array, size_t size);
-size_t partition(int array[], size_t size);
-void print_sort(int array[], size_t size, int init);
-
-/* Prototypes for advanced tasks */
+void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
@@ -36,12 +35,5 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
-/* Prototypes for print functions */
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
 
-/* Prototypes for helper functions */
-void swap(int *a, int *b);
-void heapify(int *array, int end, int start, size_t size);
-
-#endif
+#endif /*SORT_H*/
